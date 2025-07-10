83a38f9c42f8fe74ca9b12c9505ccbd3e4e0dc19
abelter <andrew.belter@gmail.com>
2025-07-09 20:10:36 -0400

    Meta file for update alerts in WME

---
+++ WME-NCDOT-Reports.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         WME NCDOT Reports
+// @namespace    https://greasyfork.org/users/45389
+// @version      2025.07.07.00
+// @description  Display NC transportation department reports in WME.
+// @author       MapOMatic, The_Cre8r, and ABelter
+// @license      GNU GPLv3
+// @include      /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/?.*$/
+// @require      https://greasyfork.org/scripts/24851-wazewrap/code/WazeWrap.js
+// @require      https://cdn.jsdelivr.net/npm/@turf/turf@7/turf.min.js
+// @require      https://update.greasyfork.org/scripts/509664/WME%20Utils%20-%20Bootstrap.js
+// @grant        GM_xmlhttpRequest
+// @connect      ncdot.gov
+// @connect      services.arcgis.com
+// @downloadURL  https://github.com/TheCre8r/WME-NCDOT-Reports/raw/master/WME-NCDOT-Reports.user.js
+// @updateURL    https://github.com/TheCre8r/WME-NCDOT-Reports/raw/master/WME-NCDOT-Reports.meta.js
+// ==/UserScript==
